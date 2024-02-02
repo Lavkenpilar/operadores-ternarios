c58d748ba1dfd51c1a5aa01e7a0689f725d7cb38
Lavken <lavkenpilar@gmail.com>
2024-02-02 01:23:40 -0300

    create-index-js

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+/*if (1+1 === "2") {
+    console.log ("Son igualitos")
+} else {
+    console.log ("De nuevo, no es lo mismo Number que String")
+}*/
+/* la declaración anterior se puede escribir de la siguiente forma con operadores ternarios, en donde if se representa con ? y else se represena con : */
+1+1 == "2"
+? console.log ("son igualitos")
+: console.log ("De nuevo, no es lo mismo un Number que un String")
